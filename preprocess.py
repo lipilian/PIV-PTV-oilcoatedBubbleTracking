2aa9dac87eaad8b41b87ef27beec411761b3f212
Hong <liuhong2@illinois.edu>
2020-09-23 10:31:15 -0500

    pre

--- preprocess.py
+++ preprocess.py
@@ -21,7 +21,20 @@ for k in tqdm(range(len(fileNames))):
     rawImagePath = os.path.join(rawPath, filename.split('-')[0], 'RawData')
     LeftImagePath = sorted(glob.glob(rawImagePath + '/*LA.tif'))
     RightImagePath = sorted(glob.glob(rawImagePath + '/*RA.tif'))
-    Left = [cv2.imread(LeftImagePath[i], -1) for i in range(startFrame, endFrame + 1)]
+    validLeftPath = []
+    validRightPath = []
+    for i in range(startFrame, endFrame + 1):
+        ss = '%06d' % i
+        for m, path in enumerate(LeftImagePath):
+            if ss in path:
+                validLeftPath.append(path)
+        for m, path in enumerate(RightImagePath):
+            if ss in path:
+                validRightPath.append(path)
+    validLeftPath = sorted(validLeftPath)
+    validRightPath = sorted(validRightPath)
+
+    Left = [cv2.imread(pp, -1) for pp in validLeftPath]
     Left = np.stack(Left, axis = 2)
     LeftBackground = np.min(Left, axis = 2).astype(np.uint16)
     for i in range(Left.shape[-1]):
@@ -29,7 +42,7 @@ for k in tqdm(range(len(fileNames))):
         ImgSavingPath = os.path.join('data',filename.split('-')[0],filename.split('-')[1],'Left%04d.tif' % i)
         cv2.imwrite(ImgSavingPath, tempImg)
 
-    Right = [cv2.imread(RightImagePath[i], -1) for i in range(startFrame, endFrame + 1)]
+    Right = [cv2.imread(pp, -1) for pp in validRightPath]
     Right = np.stack(Right, axis = 2)
     RightBackground = np.min(Right, axis = 2).astype(np.uint16)
     for i in range(Right.shape[-1]):
